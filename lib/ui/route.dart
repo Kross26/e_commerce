fc697c019f12b4def0ca9521fc623e86e38daec8
Diego Rios <riosdiego498@gmail.com>
2024-03-18 12:52:11 -0300

    routes

---
+++ lib/ui/route.dart
@@ -0,0 +1,9 @@
+import 'package:e_commerce/ui/screens.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(routes: [
+  GoRoute(
+    path: '/',
+    builder: (context, state) => const HomeScreen(),
+  )
+]);
