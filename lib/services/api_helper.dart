cdfdf598cf55ce9d08a2cc165ba90bf8d90a7ea3
Diego Rios <riosdiego498@gmail.com>
2024-03-20 14:32:59 -0300

    adding api helper

---
+++ lib/services/api_helper.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:e_commerce/services/api_service.dart';
+import 'package:quickalert/quickalert.dart';
+import 'package:quickalert/widgets/quickalert_dialog.dart';
+import '../domain/entities/product_detail.dart';
+
+class ApiHelper {
+  static Future<List<ProductDetail>> getProduct(BuildContext context) async {
+    try {
+      final response = await http
+          .get(Uri.parse(productUrl)); // hace la solicitud HTTP a la API
+      final data = jsonDecode(
+          response.body); // se codifica la respuesta utilizando jsonDecode
+      print("data: $data");
+      // se utiliza el map en la lista de datos decodificados para convertir cada elemento en un objeto "ProductDetail"
+      // utilizando fromJson, luego se convierte en una lista utilizando "toList" y se devuelve como funcion "_getProduct"
+      return data.map<ProductDetail>((e) => ProductDetail.fromJson(e)).toList();
+    } catch (e) {
+      print("error: $e");
+      QuickAlert.show(
+        context: context,
+        type: QuickAlertType.error,
+        title: 'Oops...',
+        text: 'error: $e',
+      );
+      rethrow;
+    }
+  }
+}
