12cadfa3b150a121ad350140d4c4b431cdb0940c
Diego Rios <riosdiego498@gmail.com>
2024-03-22 14:55:06 -0300

    adding floatingactionbutton

--- lib/ui/screens/product_detail_screen.dart
+++ lib/ui/screens/product_detail_screen.dart
@@ -1,15 +1,23 @@
-import 'package:e_commerce/domain/entities/product_detail.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:e_commerce/ui/screens.dart';
 import 'package:flutter/material.dart';
 
-class ProductDetailScreen extends StatelessWidget {
+class ProductDetailScreen extends StatefulWidget {
   const ProductDetailScreen({super.key, required this.product});
 
   final ProductDetail product;
 
   @override
+  State<ProductDetailScreen> createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
+  @override
   Widget build(BuildContext context) {
     double screenWidht = MediaQuery.of(context).size.width;
     return Scaffold(
+      //TODO AGREGAR FLOATINGACTIONBUTTON :::)))))
+      // CounterButton(icon: (Icons.add), onPressed: () {},),
       backgroundColor: Colors.white,
       body: SafeArea(
         bottom: false,
@@ -22,10 +30,18 @@ class ProductDetailScreen extends StatelessWidget {
                 IconButton(
                   onPressed: () {},
                   icon: const Icon(Icons.shopping_bag_outlined),
-                )
+                ),
               ],
             ),
-            Expanded(child: Image.network("${product.image}")),
+            Expanded(
+              child: CachedNetworkImage(
+                width: screenWidht * 0.9,
+                imageUrl: widget.product.image,
+                progressIndicatorBuilder: (context, url, downloadProgress) =>
+                    CircularProgressIndicator(value: downloadProgress.progress),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              ),
+            ),
             Container(
               padding: const EdgeInsets.all(20),
               height: MediaQuery.of(context).size.height * 0.5,
@@ -37,33 +53,35 @@ class ProductDetailScreen extends StatelessWidget {
                 ),
                 color: Colors.grey.shade300,
               ),
-              child: Column(
-                children: [
-                  Text(
-                    "${product.title}",
-                    maxLines: 2,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: screenWidht * 0.04,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Text(
+                      widget.product.title,
+                      maxLines: 2,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: screenWidht * 0.04,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 3),
-                  Text(
-                    "\$${product.price}",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: screenWidht * 0.03,
+                    const SizedBox(height: 3),
+                    Text(
+                      "\$${widget.product.price}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: screenWidht * 0.03,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 3),
-                  Text(
-                    "${product.description}",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: screenWidht * 0.032,
+                    const SizedBox(height: 3),
+                    Text(
+                      widget.product.description,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: screenWidht * 0.032,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             )
           ],
