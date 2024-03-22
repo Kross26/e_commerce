25cf275e33a4e85c5a0dd986b86f5754c42f888d
Diego Rios <riosdiego498@gmail.com>
2024-03-22 14:55:29 -0300

    added productbycategory

--- lib/services/api_helper.dart
+++ lib/services/api_helper.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:e_commerce/services/api_service.dart';
 import 'package:quickalert/quickalert.dart';
-import '../domain/entities/product_detail.dart';
+import '../ui/screens.dart';
 
 class ApiHelper {
   static Future<List<ProductDetail>> getProduct(BuildContext context) async {
@@ -12,13 +12,14 @@ class ApiHelper {
           .get(Uri.parse(productUrl)); // hace la solicitud HTTP a la API
       final data = jsonDecode(
           response.body); // se codifica la respuesta utilizando jsonDecode
-      print("data: $data");
+      // print("data: $data");
       // se utiliza el map en la lista de datos decodificados para convertir cada elemento en un objeto "ProductDetail"
       // utilizando fromJson, luego se convierte en una lista utilizando "toList" y se devuelve como funcion "_getProduct"
       return data.map<ProductDetail>((e) => ProductDetail.fromJson(e)).toList();
     } catch (e) {
-      print("error: $e");
+      // print("error: $e");
       QuickAlert.show(
+        // ignore: use_build_context_synchronously
         context: context,
         type: QuickAlertType.error,
         title: 'Oops...',
@@ -37,8 +38,32 @@ class ApiHelper {
           response.body); // se codifica la respuesta utilizando jsonDecode
       return data.map<String>((e) => e.toString()).toList();
     } catch (e) {
-      print("error: $e");
+      // print("error: $e");
       QuickAlert.show(
+        // ignore: use_build_context_synchronously
+        context: context,
+        type: QuickAlertType.error,
+        title: 'Oops...',
+        text: 'error: $e',
+      );
+      rethrow;
+    }
+  }
+
+  static Future<List<ProductDetail>> getProductbyCategory(
+      BuildContext context, String categoryName) async {
+    try {
+      String url = "https://fakestoreapi.com/products/category/$categoryName";
+      final response =
+          await http.get(Uri.parse(url)); // hace la solicitud HTTP a la API
+      final data = jsonDecode(
+          response.body); // se codifica la respuesta utilizando jsonDecode
+      // print("data: $data");
+      return data.map<ProductDetail>((e) => ProductDetail.fromJson(e)).toList();
+    } catch (e) {
+      // print("error: $e");
+      QuickAlert.show(
+        // ignore: use_build_context_synchronously
         context: context,
         type: QuickAlertType.error,
         title: 'Oops...',
