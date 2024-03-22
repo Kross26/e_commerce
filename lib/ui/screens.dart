03b8499ec394b98e45172abd5815a2b92955e5ac
Diego Rios <riosdiego498@gmail.com>
2024-03-22 14:55:58 -0300

    more archives exports

--- lib/ui/screens.dart
+++ lib/ui/screens.dart
@@ -1,3 +1,8 @@
+export 'package:e_commerce/ui/widgets/counter_button_cart.dart';
+
+export 'package:e_commerce/services/api_helper.dart';
+export 'package:e_commerce/ui/widgets/product_card_widget.dart';
+export 'package:e_commerce/domain/entities/product_detail.dart';
 export 'package:e_commerce/ui/views/home_view.dart';
 export 'package:e_commerce/ui/views/profile_view.dart';
 export 'package:e_commerce/ui/views/cart_view.dart';
