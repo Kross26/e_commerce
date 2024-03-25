2c6624c6bb44bf419bdc9029f8510ec26617fa86
Diego Rios <riosdiego498@gmail.com>
2024-03-25 15:38:53 -0300

    working

--- lib/ui/views/cart_view.dart
+++ lib/ui/views/cart_view.dart
@@ -1,10 +1,39 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
+import '../screens.dart';
+
 class CartView extends StatelessWidget {
-  const CartView({super.key});
+  const CartView({super.key, required this.product});
+
+  final ProductDetail product;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    double screenWidht = MediaQuery.of(context).size.width;
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            CachedNetworkImage(
+              width: screenWidht * 0.9,
+              imageUrl: product.image,
+              progressIndicatorBuilder: (context, url, downloadProgress) =>
+                  CircularProgressIndicator(value: downloadProgress.progress),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+            Row(
+              children: [
+                Text(
+                  "\$${product.title}",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: screenWidht * 0.03,
+                  ),
+                )
+              ],
+            )
+          ],
+        ));
   }
 }
