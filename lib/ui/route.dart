96aa8f9f5f0431ff3acbcfff95dcccbbb7fab882
Diego Rios <riosdiego498@gmail.com>
2024-05-24 16:36:59 -0400

    :construction: in progress

--- lib/ui/route.dart
+++ lib/ui/route.dart
@@ -4,22 +4,22 @@ import 'package:go_router/go_router.dart';
 final appRouter = GoRouter(routes: [
   GoRoute(
     path: '/',
-    name: HomeScreen.name,
+    // name: HomeScreen.name,
     builder: (context, state) => const HomeScreen(),
   ),
   GoRoute(
     path: '/cart_view',
-    name: CartView.name,
+    // name: CartView.name,
     builder: (context, state) => const CartView(),
   ),
   GoRoute(
     path: '/profile_view',
-    name: ProfileView.name,
+    // name: ProfileView.name,
     builder: (context, state) => const ProfileView(),
   ),
   GoRoute(
     path: '/home_view',
-    name: HomeView.name,
+    // name: HomeView.name,
     builder: (context, state) => const HomeView(),
   ),
 ]);
